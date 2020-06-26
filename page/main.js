b830fa2f50b0b633d4ea4c94b3aaab360c39df6f
Wolverin-e <patel.6@iitj.ac.in>
2020-06-27 02:07:14 +0530

    added basic grid drawing functionality.

--- page/main.js
+++ page/main.js
@@ -1,14 +1,32 @@
-var table = $("#grid");
+class Grid{
+    constructor(i, j){
+        this.matrix = [];
+        for(let i=1; i<100; i++){
+            this.matrix[i] = []
+            for(let j=1; j<100; j++){
+                this.matrix[i][j] = 0;
+            }
+        }
+    }
 
+    changeIntensity(i, j, table){
+        this.matrix[i][j] = 1;
+        $(table.children()[i-1].children[j-1]).css("background-color", "black")
+    }
+}
 let rows=100, coulmns=100;
 
-const td = (x, y) => {
-    let tdElem = $("<td>");
-    tdElem.data("coords", {x, y});
+let grid = new Grid(rows, coulmns);
+var table = $("#grid");
 
-    tdElem.on('click', (event) => {
-        let data = $(event.currentTarget).data("coords");
-        console.log("x:", data.x, ", y:", data.y);
+const td = (y, x) => {
+    let tdElem = $("<td draggable='true'></td>");
+    tdElem.data("coords", {x, y});
+    tdElem.width(100/coulmns+'vw');
+    tdElem.height(100/coulmns+'vw');
+    tdElem.on('dragover', (event) => {
+        // let data = $(event.currentTarget).data("coords");
+        grid.changeIntensity(y, x, table);
     })
     return tdElem;
 }
