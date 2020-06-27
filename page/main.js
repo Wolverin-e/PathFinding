d3a13df9cf88542215a3502e928072efec97534b
Wolverin-e <patel.6@iitj.ac.in>
2020-06-27 19:13:25 +0530

    some changes

--- page/main.js
+++ page/main.js
@@ -1,9 +1,9 @@
 class Grid{
-    constructor(i, j){
+    constructor(rows, cols){
         this.matrix = [];
-        for(let i=1; i<100; i++){
+        for(let i=1; i<rows; i++){
             this.matrix[i] = []
-            for(let j=1; j<100; j++){
+            for(let j=1; j<cols; j++){
                 this.matrix[i][j] = 0;
             }
         }
@@ -11,23 +11,45 @@ class Grid{
 
     changeIntensity(i, j, table){
         this.matrix[i][j] = 1;
-        $(table.children()[i-1].children[j-1]).css("background-color", "black")
+        // $(table.children()[i-1].children[j-1]).css("background-color", "black")
+        $(table.children()[i-1].children[j-1]).addClass("wallElem");
+    }
+
+    invertIntensity(initial, target){
+        const {x, y} = target.data("coords");
+        if(initial){
+            this.matrix[y][x] = 0;
+            target.removeClass("wallElem");
+        } else {
+            this.matrix[y][x] = 1;
+            target.addClass("wallElem");
+        }
     }
 }
-let rows=100, coulmns=100;
 
+
+let rows=50, coulmns=50;
 let grid = new Grid(rows, coulmns);
 var table = $("#grid");
+var initial = 0;
+
+table.on('dragover', (event) => {
+    grid.invertIntensity(initial, $(event.target));
+})
+table.on('click', (event) => {
+    grid.invertIntensity(initial, $(event.target));
+})
+table.on('dragstart', (event) => {
+    const {x, y} = $(event.target).data("coords");
+    initial = grid.matrix[y][x];
+    grid.invertIntensity(initial, $(event.target));
+})
 
 const td = (y, x) => {
     let tdElem = $("<td draggable='true'></td>");
     tdElem.data("coords", {x, y});
     tdElem.width(100/coulmns+'vw');
     tdElem.height(100/coulmns+'vw');
-    tdElem.on('dragover', (event) => {
-        // let data = $(event.currentTarget).data("coords");
-        grid.changeIntensity(y, x, table);
-    })
     return tdElem;
 }
 
