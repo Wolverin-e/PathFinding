1c0e26fa9f8bae986a94a947cfaf7142d8a58dd3
bhushanmali772 <44090382+bhushanmali772@users.noreply.github.com>
2020-07-04 15:38:07 +0530

    Update Grid.js

--- src/PathFinding/core/Grid.js
+++ src/PathFinding/core/Grid.js
@@ -1,3 +1,5 @@
+const Node= require('./Nodes');
+
 class Grid{
 	constructor(options){
 		this.rows = options.rows;
@@ -5,10 +7,88 @@ class Grid{
 		this.startPoint = options.startPoint;
 		this.endPoint = options.endPoint;
 
-		for(let y=0; y<this.rows; y++){
-			this[y] = new Array(this.columns).fill(0);
+		for(let x=0; x<this.rows; x++){
+			for(let y=0;y<this.columns;y++){
+				this[x][y]=new Node(x,y);
+			}
 		}
 	}
-}
 
+	neighbours(Node,DigonalMovement,CrossCorner){
+
+		let neighboursList=[];
+		let left_upCorner=true;
+		let left_bottomCorner=true;
+		let right_upCorner=true;
+		let right_bottomCorner=true;
+
+		if (x!==0){
+			if (this[Node.x -1][Node.y].obstacle==false){
+					neighboursList.append(this[Node.x -1][Node.y]);
+						}
+			else{
+				if (CrossCorner==false){
+					left_upCorner=false;
+					left_bottomCorner=false;
+				}
+			}
+				}
+		if (x!==this.columns-1){
+			if (this[Node.x +1][Node.y].obstacle==false){
+				neighboursList.append(this[Node.x+1][Node.y]);
+							}
+			else{
+				if (CrossCorner==false){
+					right_upCorner=false;
+					right_bottomCorner=false;
+				}
+			}
+				}
+		if (y!==0){
+			if (this[Node.x][Node.y -1].obstacle==false){
+				neighboursList.append(this[Node.x][Node.y -1]);
+					}
+			else{
+				if (CrossCorner=false){
+					left_upCorner=false;
+					right_upCorner=false;
+				}
+			}
+				}
+		if (y!==this.rows -1){
+			if (this[Node.x][Node.y +1].obstacle==false){
+				neighboursList.append(this[Node.x][Node.y +1]);
+					}
+			else{
+				if (CrossCorner=false){
+					left_bottomCorner=false;
+					right_bottomCorner=false;
+				}
+			}
+				}
+		if (DigonalMovement==true){
+			if (x!==0 && y!==0){
+				if (this[Node.x-1][Node.y -1].obstacle==false && left_upCorner==true){
+					neighboursList.append(this[Node.x-1][Node.y-1]);
+					}
+				}
+			if (x!==0 && y!==this.rows-1){
+				if (this[Node.x-1][Node.y +1].obstacle==false && left_bottomCorner==true){
+					neighboursList.append(this[Node.x-1][Node.y+1]);
+					}
+				}
+			if (x!==this.columns-1 && y!==0){
+				if (this[Node.x+1][Node.y -1].obstacle==false && right_upCorner==true){
+					neighboursList.append(this[Node.x+1][Node.y-1]);
+					}
+				}
+			if (x!==this.columns-1 && y!==this.rows-1){
+				if (this[Node.x+1][Node.y +1].obstacle==false && right_bottomCorner==true){
+					neighboursList.append(this[Node.x+1][Node.y+1]);
+					}
+				}
+			}	
+			return neighboursList;
+		}
+	}
 module.exports = Grid;
