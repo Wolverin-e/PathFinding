c168852e9a5a92b0cd1daf93a13ee64bc504005e
bhushanmali772 <44090382+bhushanmali772@users.noreply.github.com>
2020-07-06 14:20:53 +0530

    Small changes in Astar

--- src/PathFinding/algorithms/A.js
+++ src/PathFinding/algorithms/A.js
@@ -1,28 +1,16 @@
 const Heuristics=require('../core/Heurastic.js');
+const Utility= require('../core/Utility.js');
 
 class Astar{
     constructor(){
-            
+            this.G=true;
     }
-    Path(Node){
-        let path=[];
-        while (Node.parent.startpoint==false){
-                path.append(Node);
-                Node=Node.parent;
-        }
-        path1=[];
-        for (let i=Math.length(path)-1;i>-1;i--){
-            path1.append(path[i]);
-        }
-        return path1;
-
+    NoGvalueUsed(){
+        this.G=false;
     }
-
     finder(grid,given_heuristics){
-        let Heuristics=new Heuristics();
+        
         let OpenList=[];
-        let ClosedList=[];
-        let minGoalf=9999999999;
 
         grid.startpoint.startpoint=true;
         grid.endpoint.endpoint=true;
@@ -32,40 +20,47 @@ class Astar{
             let current_Node=OpenList.shift();
             let neighbours=grid.neighbours(current_Node,DiagonalMovement,CrossCorner);
             for (let i=0;i<neighbours.length;i++){
+                if (neighbours[i].visited==true){
+                    continue;
+                }
                 neighbours[i].parent=current_Node;
                 if (i<4){
-                    let dist=1;
+                    var dist=1;
                     }
                 else{
-                    let dist=1.414;
+                    var dist=1.414;
                 }
-                neighbours[i].g=neighbours[i].parent.g+dist;
+                if (this.G==false){
+                    neighbours[i].g=0;
+                }
+                else{
+                    neighbours[i].g=neighbours[i].parent.g+dist;}
                 if (given_heuristics==' Manhattan'){
                     neighbours[i].h=Heuristics.Manhattan(neighbours[i],grid.endpoint);
                 }
-                else if(given_heuristics=='Digonal'){
-                    neighbours[i].h=Heuristics.Digonal(neighbours[i],grid.endpoint);
+                else if(given_heuristics=='NoHeuristics'){
+                    neighbours[i].h=0;
+                }
+                else if(given_heuristics=='Chebyshev'){
+                    neighbours[i].h=Heuristics.Chebyshev(neighbours[i],grid.endpoint);
+                }
+                else if(given_heuristics=='Octile'){
+                    neighbours[i].h=Heuristics.Octile(neighbours[i],grid.endpoint);
                 }
                 else{
                     neighbours[i].h=Heuristics.Euclidean(neighbours[i],grid.endpoint);
                 }
                 neighbours[i].f=neighbours[i].g+neighbours[i].h;
                 if (neighbours[i].endpoint==true){
-                    if (minGoalf>neighbours[i].f){
-                        minGoalf=neighbours[i].f
-                        if (minGoalf<=OpenList[0].f){
-                            let path=this.Path(neighbours[i]);
-                            return path;
+                        let path=Utility.Path(neighbours[i]);
+                        return path;
                         }
-                    }
-                }
                 OpenList.append(neighbours[i]);
                 }
-            ClosedList.append(current_Node);
-            OpenList.sort(function(a,b){
-                return a.f-b.f;
-            });
-
+                current_Node.visited=true;
+                OpenList.sort(function(a,b){
+                return a.f-b.f;});
         }
     }
 }
+module.exports=Astar;
