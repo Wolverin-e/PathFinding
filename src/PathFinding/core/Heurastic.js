bcbfc4988e429189ef2c2e81593d26554ec67d18
bhushanmali772 <44090382+bhushanmali772@users.noreply.github.com>
2020-07-06 14:17:54 +0530

    Added octile heuristics

--- src/PathFinding/core/Heurastic.js
+++ src/PathFinding/core/Heurastic.js
@@ -6,7 +6,7 @@ class Heuristics{
         return h;
     }
 
-    Digonal(Node,EndPoint){
+    Chebyshev(Node,EndPoint){
         h=Math.max(Math.abs(Node.x-EndPoint.x),Math.abs(Node.y-EndPoint.y));
         return h;
     }
@@ -15,4 +15,9 @@ class Heuristics{
         h=Math.sqrt(Math.pow(Node.x-EndPoint.x,2)+Math.pow(Node.y-EndPoint.y,2));
         return h;
     }
+    Octile(Node,EndPoint){
+        h=Math.max(Math.abs(Node.x-EndPoint.x),Math.abs(Node.y-EndPoint.y))+(Math.sqrt(2)-1)*(Math.min(Math.abs(Node.x-EndPoint.x),Math.abs(Node.y-EndPoint.y)));
+        return h;
+    }
 }
+module.exports=Heuristics;
